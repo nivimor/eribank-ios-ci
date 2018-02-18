55c3bdf8d673259a44f4d81772310affc7b266f2
Nivi Mor <nivi.mor@experitest.com>
2018-02-18 07:20:32 +0200

    Finalized login with Email

--- scripts/merge.sh
+++ scripts/merge.sh
@@ -2,9 +2,9 @@ echo ${TRAVIS_BRANCH}
 git stash
 echo "switching to master"
 git checkout -b master
+echo "pulling master"
+git pull https://${GIT_AUTH}@github.com/nivimor/eribank-ios-ci.git master
 echo "merging ${TRAVIS_BRANCH} to master"
 git merge ${BRANCH}
-echo "pulling master"
-git pull origin master
 echo "pushing to master"
 git push https://${GIT_AUTH}@github.com/nivimor/eribank-ios-ci.git master --force
